a7719992ff29ad1064cc91a974ec6c56615452b4
Saifmod <ssyf87946@gmail.com>
2025-01-08 21:54:13 +0300

    Brookhaven-hub-skin

---
+++ تحديث ماب البيوت.lua
@@ -0,0 +1,532 @@
+_Hawk = "ohhahtuhthttouttpwuttuaunbotwo"
+local Hawk = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHanki/HawkHUB/main/LibSources/HawkLib.lua", true))()
+local Window = Hawk:Window({
+	ScriptName = "Brookhaven HUB",
+	DestroyIfExists = true, --if false, gui wont disappear
+	Theme = "Dark" --Themes: Pink, White, Dark
+})
+Window:Close({
+	visibility = true, --if false, close button will disappear
+	Callback = function()
+		Window:Destroy() --Destroying Gui Function
+	end,
+})
+local tab1 = Window:Tab("الرئيسية") 
+local newsec = tab1:Section("منور السكربت")
+newsec:Line() 
+local label = newsec:Label("New Label","Example Description") 
+label:UpdateLabel("اصدار تجريبي","سكربت سكنات ماب")
+newsec:Line() 
+newsec:Paragraph("الحقوق",{"صانع السكربت : @uurrrri","رابط القناه : @ksjskawj","شكرا لاستخدامك السكربت"})
+newsec:Line() 
+local label = newsec:Label("New Label","Example Description") 
+label:UpdateLabel("المشاكل","اي خطأ تلقاه في السكربت تواصل مع الصانع : @uurrrri")
+local tab1 = Window:Tab("قسم سكنات جاهزه")
+local newsec = tab1:Section("سكنات المشاهير")
+newsec:Line() 
+local newsec = tab1:Section("ملاحظه : كل ما تختار سكن اقتل روحك")
+local button = newsec:Button("رونالدو","اضغط لكي تلبس",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 107047655981442
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 101798107467596
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 11813475271
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 11926234580
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("ميسي",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 11875721940
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 11744913753
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 17398474153
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سبيد",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 18708360861
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 16600343226
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+local args = {
+    [1] = "wear",
+    [2] = 75456483031847
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local tab1 = Window:Tab("قسم التيشرتات")
+local newsec = tab1:Section("اختر ما تريد")
+newsec:Line() 
+local button = newsec:Button("تيشرت 1",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 10706059010
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 2",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 7020213212
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("تيشرت 3",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 6308077445
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("تيشرت 4",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 8417043268
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args)) 
+end)
+
+local button = newsec:Button("تيشرت 5",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 11455741618
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("تيشرت 6",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 13491817168
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args)) 
+end)
+
+local button = newsec:Button("تيشرت 7",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 12519496793
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args)) 
+end)
+
+local button = newsec:Button("تيشرت 8",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14056650454
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args)) 
+end)
+
+local button = newsec:Button("تيشرت 9",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 10803914145
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("تيشرت 10",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 12888689819
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args)) 
+end)
+local button = newsec:Button("تيشرت 11",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 8108996222
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 12",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 19002665257
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 13",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14752060770
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 14",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 11768583070
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 15",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 5884168216
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 16",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14501182583
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 17",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 8702288107
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 18",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 5884168216
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 19",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14917192295
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 20",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14051949294
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 21",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 13717676521
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 22",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 17786071832
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 23",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 5921799243
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 24",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14972198978
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local button = newsec:Button("تيشرت 25",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14906051506
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+end)
+local tab1 = Window:Tab("قسم السروال") 
+local newsec = tab1:Section("اختر ما تريد")
+newsec:Line() 
+local button = newsec:Button("سروال 1",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 7191535915
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 2",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 7653196601
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 3",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 16469539
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 4",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14251507523
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 5",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 87335676636705
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 6",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 9219209747
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 7",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 15339398667
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+
+local button = newsec:Button("سروال 8",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 12150726324
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 9",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 12049131738
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 10",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 11535490063
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 11",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 13602718304
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 12",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 7030419255
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 13",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 15922931533
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 14",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14740138434
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 15",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 17121380439
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 16",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 14875093258
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 17",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 9660730676
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 18",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 16535560016
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 19",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 4549491810
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 20",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 17079455513
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 21",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 18220747864
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 22",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 16639477481
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 23",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 9224010663
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 24",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 9970898886
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local button = newsec:Button("سروال 25",function()
+	print("Button")local args = {
+    [1] = "wear",
+    [2] = 10490928775
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+end)
+local tab1 = Window:Tab("قريبا") 
+local tab1 = Window:Tab("قريبا") 
+local tab1 = Window:Tab("قريبا") 
+local tab1 = Window:Tab("قريبا") 
